ddf34a475ed5cabf77ba219ca13db9a81a495ff6
hyy001 <1466187112@qq.com>
2022-08-21 17:25:04 +0800

    feat: add image components

--- src/rawComponents/image/src/image.vue
+++ src/rawComponents/image/src/image.vue
@@ -1,13 +1,53 @@
 <template>
   <img
-    src="
-  "
+    :class="['lc-image']"
+    :src="propsCopy.src"
+    :style="imageStyle"
     alt=""
+    v-bind="$attrs"
+    @click="handleImgClick"
+    draggable
+    @dragstart="handleDragstart"
+    @dragend="handleDragend"
   />
 </template>
 
-<script lang="ts">
-export default {};
+<script>
+import { ref } from 'vue';
+export default {
+  name: 'LCImage',
+  emits: ['click', 'dragstart', 'dragend'],
+  props: {
+    src: String,
+    fit: String
+  },
+  setup(props, { emit }) {
+    const propsCopy = ref({ ...props });
+    const imageStyle = ref({});
+    imageStyle.value['object-fit'] = propsCopy.value.fit;
+    const handleImgClick = (evt) => {
+      emit('click', evt);
+    };
+    const handleDragStart = (evt) => {
+      emit('dragstart', evt);
+    };
+    const handleDragend = (evt) => {
+      emit('dragend', evt);
+    };
+    return {
+      handleImgClick,
+      propsCopy,
+      handleDragStart,
+      handleDragend,
+      imageStyle
+    };
+  }
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.lc-image {
+  height: 100px;
+  width: 100px;
+}
+</style>
---
+++ src/rawComponents/image/src/mapImage.js
@@ -0,0 +1,45 @@
+export default {
+  "type": "button",
+  "data": [
+    {
+      "id": 1,
+      "src": "",
+      "style": {
+        "height": "100px",
+        "width": "100px"
+      }
+    },
+    {
+      "id": 2,
+      "src": "",
+      "style": {
+        "height": "100px",
+        "width": "100px"
+      }
+    },
+    {
+      "id": 3,
+      "src": "",
+      "style": {
+        "height": "100px",
+        "width": "100px"
+      }
+    },
+    {
+      "id": 4,
+      "src": "",
+      "style": {
+        "height": "100px",
+        "width": "100px"
+      }
+    },
+    {
+      "id": 5,
+      "src": "",
+      "style": {
+        "height": "100px",
+        "width": "100px"
+      }
+    }
+  ]
+};
