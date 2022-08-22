ed6ffbf9507f81f409d3bbff067a113b88463b63
hyy001 <1466187112@qq.com>
2022-08-22 14:15:56 +0800

    feat: add drag

--- src/components/RenderControlPanel.vue
+++ src/components/RenderControlPanel.vue
@@ -15,17 +15,69 @@
       tag="button"
       >{{ item.value }}</lc-button
     >
+    <lc-link
+      v-if="item.tag == 'link'"
+      :index="index"
+      :value="item.value"
+      :underline="item.underline"
+      :disabled="item.disabled"
+      :type="item.type"
+      :id="item.id"
+      :isview="true"
+      :coordinate="item.coordinate"
+      tag="link"
+    ></lc-link>
+    <lc-image
+      v-if="item.tag === 'image'"
+      :index="index"
+      :src="item.src"
+      :fit="item.fit"
+      :id="item.id"
+      :isview="true"
+      :coordinate="item.coordinate"
+      tag="image"
+    ></lc-image>
+    <lc-video
+      v-if="item.tag === 'video'"
+      :loop="item.loop"
+      :src="item.src"
+      :height="item.height"
+      :width="item.width"
+      :index="index"
+      :id="item.id"
+      :isview="true"
+      :coordinate="item.coordinate"
+      tag="video"
+    ></lc-video>
+    <lc-text
+      v-if="item.tag === 'text'"
+      :type="item.type"
+      :value="item.value"
+      :index="index"
+      :id="item.id"
+      :isview="true"
+      :coordinate="item.coordinate"
+      tag="text"
+    ></lc-text>
   </div>
 </template>
 
 <script>
 import lcButton from '../rawComponents/button';
+import lcLink from '@/rawComponents/link';
+import lcImage from '@/rawComponents/image';
+import lcVideo from '@/rawComponents/video';
+import lcText from '@/rawComponents/text';
 import { useStore } from 'vuex';
 import { computed } from 'vue-demi';
 export default {
   name: 'RenderControlPanel',
   components: {
-    lcButton
+    lcButton,
+    lcLink,
+    lcImage,
+    lcVideo,
+    lcText
   },
   setup() {
     const store = useStore();
--- src/rawComponents/image/src/image.vue
+++ src/rawComponents/image/src/image.vue
@@ -1,44 +1,97 @@
 <template>
-  <img :class="['lc-image']" :src="propsCopy.src" :style="imageStyle" alt="" v-bind="$attrs" @click="handleImgClick" draggable="true" @dragstart="handleDragstart" @dragend="handleDragend" />
+  <img
+    :style="[positionStyle, imageStyle]"
+    :class="['lc-image']"
+    :src="propsCopy.src"
+    alt=""
+    v-bind="$attrs"
+    @click="transferStyleInfo(propsCopy.index)"
+    draggable="true"
+    @dragstart="dragStart($event)"
+    @dragend="isViewOrComponent($event)"
+  />
 </template>
 
 <script>
-  import { ref } from 'vue';
-  export default {
-    name: 'LCImage',
-    emits: ['click', 'dragstart', 'dragend'],
-    props: {
-      src: String,
-      fit: String
-    },
-    setup(props, { emit }) {
-      const propsCopy = ref({ ...props });
-      const imageStyle = ref({});
-      imageStyle.value['object-fit'] = propsCopy.value.fit;
-      const handleImgClick = (evt) => {
-        emit('click', evt);
-      };
-      const handleDragstart = (evt) => {
-        emit('dragstart', evt);
-      };
-      const handleDragend = (evt) => {
-        emit('dragend', evt);
-      };
-      return {
-        handleImgClick,
-        propsCopy,
-        handleDragstart,
-        handleDragend,
-        imageStyle
-      };
+import { ref, computed } from 'vue';
+import useDrag from '../../../hooks/useDrag';
+import useStyle from '../../../hooks/useStyle';
+export default {
+  name: 'LCImage',
+  emits: ['click', 'dragstart', 'dragend'],
+  props: {
+    src: String,
+    fit: String,
+    isview: Boolean,
+    id: Number,
+    value: String,
+    tag: String,
+    coordinate: Array,
+    style: Object,
+    index: Number
+  },
+  setup(props, { emit }) {
+    const propsCopy = ref({ ...props });
+    const imageStyle = ref({});
+    imageStyle.value['object-fit'] = propsCopy.value.fit;
+    const { dragToView, dragStart, dragEnd } = useDrag();
+    const { transferStyleInfo } = useStyle();
+    const x = computed({
+      get() {
+        return props.isview ? props.coordinate[0] : null;
+      },
+      set() {
+        return props.isview ? props.coordinate[0] : null;
+      }
+    });
+
+    const y = computed({
+      get() {
+        return props.coordinate ? props.coordinate[1] : null;
+      },
+      set() {
+        return props.coordinate ? props.coordinate[1] : null;
+      }
+    });
+
+    const positionStyle = computed({
+      get() {
+        // console.log('here');
+        return x.value && y.value
+          ? `left:${x.value}px;top:${y.value}px;position:absolute`
+          : '';
+      },
+      set() {
+        return x.value && y.value
+          ? `left:${x.value}px;top:${y.value}px;position:absolute`
+          : '';
+      }
+    });
+
+    //看是在预览区拖拽还是在组件区拖拽
+    function isViewOrComponent($event) {
+      return props.isview ? dragEnd($event) : dragToView($event, propsCopy);
     }
-  };
+    return {
+      x,
+      y,
+      positionStyle,
+      propsCopy,
+      isViewOrComponent,
+      transferStyleInfo,
+      dragToView,
+      dragStart,
+      dragEnd,
+      imageStyle
+    };
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-  .lc-image {
-    height: 100px;
-    width: 100px;
-    margin: 20px;
-  }
+.lc-image {
+  height: 100px;
+  width: 100px;
+  margin: 20px;
+}
 </style>
